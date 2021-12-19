7aaaf33bf8215cd077fb8be30483d270e5cd3465
Gal <galpalas89@gmail.com>
2021-12-19 18:53:04 +0200

    add spinner component

--- src/components/hooks/useFetchCharacters.js
+++ src/components/hooks/useFetchCharacters.js
@@ -2,16 +2,18 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const useFetchCharacters = () => {
+  const [status, setStatus] = useState(false);
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     const fetchCharacters = async () => {
       const { data } = await axios("https://rickandmortyapi.com/api/character");
       setCharacters(data.results);
+      setStatus(true);
     };
 
     fetchCharacters();
   }, []);
 
-  return { characters };
+  return { characters, status };
 };
