fef52658e95e6e256ffec0f96fe5604e3e701da0
Gal <galpalas89@gmail.com>
2021-12-21 18:27:39 +0200

    create footer component

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer className="position-relative bg-light text-center p-3">
+      <div className="container align-items-center">
+        <p className="lead" style={{ color: "darkcyan" }}>
+          Copyright &copy; 2021
+        </p>
+        <a
+          href="/#"
+          className="position-absolute bottom-0 end-0 h1 text-info mx-2 p-3"
+        >
+          <i class="bi bi-arrow-up-circle-fill"></i>
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
